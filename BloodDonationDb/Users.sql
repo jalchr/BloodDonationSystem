c5c82dcb2e79adb004fe3f0971c0c058f12b3b96
HasanShmsdn <HasanShamseddin@hotmail.com>
2015-06-15 01:16:04 +0300

    remove unused files

--- BloodDonationDb/Users.sql
+++ BloodDonationDb/Users.sql
@@ -1,7 +1,11 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [Username] NVARCHAR(50) NULL, 
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserName] NVARCHAR(50) NULL, 
     [Password] NVARCHAR(50) NULL, 
-    [Lastlogin] DATETIME NULL
+    [Lastlogin] DATETIME NULL, 
+    [IsAdmin] BIT NULL, 
+    [HospitalName] NVARCHAR(50) NULL, 
+    [HospitalLocation] NVARCHAR(200) NULL, 
+    [Role] NVARCHAR(50) NULL
 )
